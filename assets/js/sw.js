53e2f9130e56bf0c87d7b3f2d5c5d99d158bda19
xisonc <cory@evolutionmedia.ca>
2022-10-13 10:56:09 -0600

    Delete sw.js
    moved to root folder

--- assets/js/sw.js
+++
@@ -1,4 +0,0 @@
-// absolute bare minimum to trigger install
-self.addEventListener('install', function(e){return;});
-self.addEventListener('activate', function(e){return;});
-self.addEventListener('fetch', function(e){return;});
