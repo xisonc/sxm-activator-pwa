0ecb4b9a3142360a5d7e12645ed69e5df8bebd81
xisonc <cory@evolutionmedia.ca>
2022-10-12 23:22:05 -0600

    Create sw.js

---
+++ assets/js/sw.js
@@ -0,0 +1,4 @@
+// absolute bare minimum to trigger install
+self.addEventListener('install', function(e){return;});
+self.addEventListener('activate', function(e){return;});
+self.addEventListener('fetch', function(e){return;});
